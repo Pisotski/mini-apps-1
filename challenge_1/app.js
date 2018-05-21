d44ee24c12307ed90da894ccd41a1ed0b28ec1bd
Poisk <vpisotski@gmail.com>
2018-05-21 16:54:41 -0700

    players toggle each turn with different token

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,4 @@
-var info = [{player1: 'x'}, {player2: 'o'}];
+var info = [{player: 'player one', token: '[X]'}];
 
 //onClick of any [] place el 'x'
 
@@ -8,11 +8,35 @@ var info = [{player1: 'x'}, {player2: 'o'}];
 
 //create function that checks
 
+
+var rules = {
+  playerTurn: function() {
+    let player = info[0].player;
+    let token = info[0].token;
+    if (info[0].player === 'player one') {
+      info[0].player = 'player two';
+      info[0].token = '[O]'
+    } else {
+      info[0].player = 'player one';
+      info[0].token = '[X]'
+    }
+    return token;
+  },
+  findPlace: function(place) {
+
+  }
+} 
+
 var handlers = {
   checkBox: function(event) {
     document.addEventListener("click", function(){
-      event.target.innerHTML = '[x]'
-        console.log(event.target.innerHTML);
+      var className = event.target.className.slice(event.target.className.length - 8);
+        if (event.target.innerHTML.length <= 2) {
+        event.target.innerHTML = rules.playerTurn();
+        }
+        var node = document.getElementsByClassName(className);
+        console.log(node[0].attributes[1] = '');
+        node[0].attributes[1] = ''
     });
   }
 }
