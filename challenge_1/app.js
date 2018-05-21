9b44baaac2e3ab2168836e16c71eee251552a4d3
Poisk <vpisotski@gmail.com>
2018-05-21 15:27:47 -0700

    created pseudoboard with ability to change innerhtml

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,18 @@
+var info = [{player1: 'x'}, {player2: 'o'}];
 
+//onClick of any [] place el 'x'
 
+//onClick of first [] place el 'x' change el 'x' to el 'o'
+
+//create function that pushes el into correct row, column or diagonal
+
+//create function that checks
+
+var handlers = {
+  checkBox: function(event) {
+    document.addEventListener("click", function(){
+      event.target.innerHTML = '[x]'
+        console.log(event.target.innerHTML);
+    });
+  }
+}
