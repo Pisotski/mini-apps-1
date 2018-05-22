f877b513c98d28ee362fd3e26495422191573cd6
Poisk <vpisotski@gmail.com>
2018-05-21 18:36:46 -0700

    created a normal board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,4 @@
-var info = [{player: 'player one', token: '[X]'}];
+var info = [{player: 'player one', token: 'X'}];
 
 //onClick of any [] place el 'x'
 
@@ -8,35 +8,51 @@ var info = [{player: 'player one', token: '[X]'}];
 
 //create function that checks
 
-
 var rules = {
   playerTurn: function() {
     let player = info[0].player;
     let token = info[0].token;
     if (info[0].player === 'player one') {
       info[0].player = 'player two';
-      info[0].token = '[O]'
+      info[0].token = 'O'
     } else {
       info[0].player = 'player one';
-      info[0].token = '[X]'
+      info[0].token = 'X'
     }
     return token;
   },
-  findPlace: function(place) {
-
-  }
 } 
 
 var handlers = {
+  createBoard: function() {
+    document.addEventListener("click", function(){
+      board();
+    });
+  },
   checkBox: function(event) {
     document.addEventListener("click", function(){
       var className = event.target.className.slice(event.target.className.length - 8);
-        if (event.target.innerHTML.length <= 2) {
-        event.target.innerHTML = rules.playerTurn();
+        if (event.target.innerHTML.length <= 0) {
+          event.target.innerHTML = rules.playerTurn();
         }
-        var node = document.getElementsByClassName(className);
-        console.log(node[0].attributes[1] = '');
-        node[0].attributes[1] = ''
     });
   }
+}
+
+var board = function(number=3) {
+  var brd = document.getElementById("brd");
+  for (var r = 0; r < number; r++) {
+    var row = document.createElement("tr");
+    for (var c = 0; c < number; c++) {
+      var cell = document.createElement("td");
+      cell.width = "50px";
+      cell.height = "50px";
+      cell.id = `c${c}r${r}`;
+      cell.onclick = handlers.checkBox;
+      row.appendChild(cell);
+    }
+    info.push([[],[],[]]);
+    brd.appendChild(row);
+  }
+      console.log(info);
 }
